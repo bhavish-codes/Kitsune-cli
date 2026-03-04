1a7fc3fe5108c9f0152a0a60788411e87a72704b
banned-from-everywhere <bhavishdhar@gmail.com>
2026-03-05 01:55:22 +0530

    implimented Help

--- commands/help.ts
+++ commands/help.ts
@@ -1,16 +1,23 @@
 class Help{
     program;
+    existingCommands = ["spirit", "speak", "riddle", "joke", "loud", "whisper"];
+    implimentedCommands = [];
     constructor(program){
         this.program = program;
     }
     register(){
         this.program
         .command("assist <name>")
-        .action((name=>this.sayHelp(name) ))
+        .action((name=>this.show(name) ))
     }
-    sayHelp(name){
-        console.log(`How can I assist you, ${name}?`);
+    show(name){
+        if (this.implimentedCommands.includes(name)){
+            console.log(`The command '${name}' is available.`);
+        } else if (this.existingCommands.includes(name)){
+            console.log(`The command '${name}' is still in development. Please check back later!`);
+        }else{
+            console.log(`The command '${name}' is not available.`);
+        }
     }
-
 }
 export default Help;
